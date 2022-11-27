582e3c8c78f1476eb68fcbd9fdfd1081da2ba804
VinitK <vinit.kumar_1@signify.com>
2022-11-27 15:39:14 +0530

    First commit to the master branch.

--- udemy_flutter_section2a/lib/main.dart
+++ udemy_flutter_section2a/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import './answer.dart';
-import './question.dart';
-
-// import 'package:udemy_flutter_section2a/question.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,39 +8,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questions = [
-    'What\'s your favourite color?',
-    'What\'s your favourite animal?',
-  ];
-
-  var questionIndex = 0;
-
-  void _answerQuestion() {
-    print("questionIndex Start-> \t$questionIndex");
-    setState(() {
-      if ((questionIndex + 1) >= questions.length)
-        questionIndex = 0;
-      else
-        questionIndex++;
-
-      print("questionIndex End-> \t$questionIndex");
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              Question(questions[questionIndex]),
-              Answer(_answerQuestion),
-            ],
-          ),
+          child: Text("This is the body of the Scaffold Widget!"),
         ),
       ),
     );
